015dfd6fc9cb69feed7409d6c20236d099a07b95
Darylabrador <abrador.daryl@gmail.com>
2020-10-01 12:54:08 +0400

    Add : handle dropdown menu

--- public/js/dropdownHandlers.js
+++ public/js/dropdownHandlers.js
@@ -1,14 +1,22 @@
 $(function(){
-    var dropdown = document.getElementsByClassName("dropdown-btn");
+    var dropdown = document.querySelectorAll(".dropdown-btn");
 
-    for (let i = 0; i < dropdown.length; i++) {
-        dropdown[i].addEventListener("click", function () {
-            var dropdownContent = this.nextElementSibling;
-            if (dropdownContent.style.display === "block") {
-                dropdownContent.style.display = "none";
-            } else {
-                dropdownContent.style.display = "block";
-            }
+    function hideAll() {
+        var dropdownContainer = document.querySelectorAll(".dropdown-container");
+        dropdownContainer.forEach(btn => {
+            btn.style.display = "none";
         });
-    } 
+    }
+
+    dropdown.forEach(btn => {
+        btn.addEventListener('click', (evt) => {
+            if (evt.currentTarget.nextElementSibling.style.display === "block") {
+                hideAll();
+                evt.currentTarget.nextElementSibling.style.display = "none";
+            } else {
+                hideAll();
+                evt.currentTarget.nextElementSibling.style.display = "block";
+            }
+        })
+    })
 })
